b7b5f549efe529353e3f12de661e806dedd3f0dc
Rajat Singh <singhr3234@gmail.com>
2023-06-06 23:09:59 +0530

    Question 1 is added

---
+++ Question_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Question_1{
+    static int occurence(int[]arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(occurence(arr));
+        
+    }
+}
